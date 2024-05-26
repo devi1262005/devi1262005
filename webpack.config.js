dc1d492ef81a34087de0dc2cb4fe19ebf72a71cc
devi1262005 <sreehehe@gmail.com>
2024-05-27 04:28:48 +0530

    A random blog in making

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'node_modules')],
+  },
+};
